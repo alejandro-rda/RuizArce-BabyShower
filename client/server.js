e9b032440d30eb6b1f35b219d60ab2de2c35423b
Alejandro Ruiz <_lruizda@falabella.com.pe>
2019-09-12 15:50:34 -0500

    Create new Server CLI

---
+++ client/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist/babyshower-ruizarce'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/dist/babyshower-ruizarce/index.html'));
+});
+app.listen(process.env.PORT || 8080);
