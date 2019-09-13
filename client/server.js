13343fe46c9680dd0ad42a14a383f466b9d4d8fb
Alejandro Ruiz <_lruizda@falabella.com.pe>
2019-09-13 11:49:24 -0500

    Config Angular

--- client/server.js
+++ client/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/babyshower-ruizarce/src'));
+app.use(express.static('./dist/babyshower-ruizarce'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/babyshower-ruizarce/src/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/babyshower-ruizarce/index.html'));
 });
 app.listen(process.env.PORT || 8080);
